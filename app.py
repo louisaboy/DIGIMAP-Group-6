f9eabcf61b4a54e0a87b8874845b7c8f5daca601
izskat <73699431+izskat@users.noreply.github.com>
2022-07-02 05:25:31 +0800

    Image preview and new html

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def upload_image():
         file.save(filepath)
         #print('upload_image filename: ' + filename)
         flash('Image successfully uploaded')
-        return render_template('index.html', filename=filename, base64=generate_images(filepath))
+        return render_template('new-index.html', filename=filename, base64=generate_images(filepath))
     else:
         flash('Allowed image types are - png, jpg, jpeg')
         return redirect(request.url)
